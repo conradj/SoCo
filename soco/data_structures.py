d8fedaa306487e26b554af0ccc87a95100fe121d
conradj <conradsjackson@gmail.com>
2015-10-12 22:29:28 +0100

    keep trying

--- soco/data_structures.py
+++ soco/data_structures.py
@@ -890,7 +890,7 @@ class DidlMusicAlbumFavorite(DidlAlbum):
     # to be an error by Sonos.
     tag = 'item'
     
-class DidlPlaylistFavorite(DidlPlaylist):
+class DidlPlaylistFavorite(DidlPlaylistContainer):
 
     """Class that represents a Sonos favorite music playlist.
     Added by Conrad Jackson (so that I can search for it when it breaks)
@@ -900,7 +900,10 @@ class DidlPlaylistFavorite(DidlPlaylist):
     print ("conrad was ere")
     # the DIDL Lite class for this object.
     item_class = 'object.container.playlistContainer.sonos-favorite'
-    
+    # Despite the fact that the item derives from object.container, it's
+    # XML does not include a <container> tag, but an <item> tag. This seems
+    # to be an error by Sonos.
+    tag = 'item'
 
 
 class DidlMusicAlbumCompilation(DidlAlbum):
